763b2fdeb8223ccb536cae3ccfb395e74b3989b3
Elvin Ramirez <102904220+ElvinRamirez@users.noreply.github.com>
2022-07-29 12:08:13 -0400

    Counter assignment

--- flask/fundamentals/Pets/server.py
+++ flask/fundamentals/Pets/server.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, session,redirect,request  
 app = Flask(__name__)
 from data import trainingSchool
+app.secret_key = "Josh is the man today"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user' not in session:
+        return render_template('logReg.html')
+    else:
+        return render_template('index.html, cohorts=trainingSchool')
+
+#This is a hidden route
+@app.route('/createUser', method=['post'])
+def createUSer():
+    session['user'] = request.form['fullName']
+    return redirect('/')
+
+@app.route('/cohort/<int:cohort_id>/view')
+def viewCohort(cohort_id):
+    if 'user' not in session:
+        return render_template('logReg.html')
+    else:
+        return render_template('index.html, cohorts=trainingSchool')
 
 if __name__=="__main__":    
     app.run(debug=True)
---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,27 @@
+from crypt import methods
+from flask import Flask, render_template,session,redirect,request
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+
+@app.route("/")
+def index():
+    #count defines the session in index.html
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session['count'] += 2
+    return render_template("index.html")
+
+@app.route("/destroy_session")
+def destroy():
+    session.clear()
+    return redirect("/")
+
+@app.route("/increment", methods=["POST"])
+def increment():
+    session['count'] += int(request.form['increment'])
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
